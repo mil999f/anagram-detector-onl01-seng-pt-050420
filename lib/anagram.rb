a08eeda953cd0d8d37506af5bfe2d6d2e483ed77
Milly Flores <miflo0h@gmail.com>
2020-06-10 00:06:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+
+  attr_accessor :array_words
+
+  
+  def initialize(array_words)
+    @array_words = array_words 
+  end
+  
+  def match(find_this)
+    find_this.find_all do |word|
+      if word.split("").sort == self.array_words.split("").sort
+        word
+      end
+    end
+  end
+
+end
